5d705fb78a6b726a97ef41d08fd5a9bc8235101a
ZAZEKAPU <karaoke.zazekapu@gmail.com>
2023-09-14 20:10:43 +0700

    update first project

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Hello vuejs3</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos11App',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
